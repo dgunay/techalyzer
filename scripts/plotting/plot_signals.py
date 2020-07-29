c07d8390a5ddada19ac1363797e668fbc3025419
dgunay <devingunay@gmail.com>
2020-07-28 17:57:53 -0700

    stub plotting script

---
+++ scripts/plotting/plot_signals.py
@@ -0,0 +1,8 @@
+# TODO: ingest JSON, plot
+
+# Signals objects are usually like this, with some exceptions:
+# {
+#   "outputs" : [array of floats],
+#   "prices"  : [array of floats],
+#   "signals" : [array of floats]
+# }
