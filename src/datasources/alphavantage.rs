ddce8a5d6fae2bda8a42b131e7e6b91a6547affb
dgunay <devingunay@gmail.com>
2020-08-11 15:59:58 -0700

    add fixme comment to alphavantage

--- src/datasources/alphavantage.rs
+++ src/datasources/alphavantage.rs
@@ -19,6 +19,9 @@ impl DataSource for AlphaVantage {
     fn get(&self, symbol: &str) -> Result<Prices, Error> {
         // TODO: if start is in the last 100 market days, don't request the full
         // time series datas
+
+        // FIXME: this doesn't use adjusted close, consider submitting a PR for
+        // that.
         match self.client.get_time_series_daily_full(symbol) {
             Ok(t) => Ok(t.into()),
             Err(e) => Err(Error::AlphaVantageError(e.to_string())),
