6cb6a0878f3201f519b8e4d2455fb6c139de1be6
dgunay <devingunay@gmail.com>
2020-08-14 16:11:46 -0700

    remove todo comment

--- src/trading/mod.rs
+++ src/trading/mod.rs
@@ -5,7 +5,6 @@ pub mod tradingmodel;
 
 use strum_macros::{Display, EnumString};
 
-// TODO: move this somewhere else
 /// Can be an ML model or a handwritten algorithm.
 #[derive(Debug, EnumString, Display)]
 pub enum SupportedTradingModel {
